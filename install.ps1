e7e15e548b54d606632363303c0db2ce514acfa2
guowei.lu <guowei.lu@aliyun.com>
2026-07-02 20:21:49 +0800

    chore(release): bump version to 0.2.2

--- install.ps1
+++ install.ps1
@@ -6,7 +6,7 @@ param(
   [string]$Agent,
 
   [Alias("version")]
-  [string]$Version = "0.2.1",
+  [string]$Version = "0.2.2",
 
   [Alias("baseUrl")]
   [string]$BaseUrl = "https://github.com/valkor-ai/loom/releases/latest/download",
